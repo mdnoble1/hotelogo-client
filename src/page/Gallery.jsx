ce0c007c6600d4034199e9bc4e25be2dba7a14b8
Md Noble <mdnoble.wd@gmail.com>
2023-11-08 21:20:13 +0600

    added gallery on homepage

--- src/page/Gallery.jsx
+++ src/page/Gallery.jsx
@@ -1,28 +1,28 @@
-// import img5 from '../assets/images/5.jpg';
-// import img2 from '../assets/images/2.jpg';
-// import img3 from '../assets/images/3.jpg';
-// import img1 from '../assets/images/1.jpg';
-// import img4 from '../assets/images/4.jpg';
+import img5 from '../assets/images/5.jpg';
+import img2 from '../assets/images/2.jpg';
+import img3 from '../assets/images/3.jpg';
+import img1 from '../assets/images/1.jpg';
+import img4 from '../assets/images/4.jpg';
 
 
 
 
 
 
-// const Gallery = () => {
-//     return (
-//         <div className='max-w-[1240px] mx-auto py-16 px-4 text-center'>
-//         <h1 className='text-4xl lg:text-5xl font-bold text-[#D31027]'>OUR GALLERY</h1>
-//         <p className='text-2xl py-4 font-bold'>Glance at our great image</p>
-//         <div className='grid grid-rows-none md:grid-cols-5 py-4 gap-2 md:gap-4'>
-//             <img data-aos="fade-right" data-aos-duration="1500" className='w-full h-full object-cover col-span-2 md:col-span-3 row-span-2' src={img1} alt="/" />
-//             <img data-aos="fade-left" data-aos-duration="1500" className='w-full h-full object-cover' src={img2} alt="/" />
-//             <img data-aos="fade-left" data-aos-duration="1500"  className='w-full h-full object-cover' src={img3} alt="/" />
-//             <img data-aos="fade-left" data-aos-duration="1500"  className='w-full h-full object-cover' src={img4} alt="/" />
-//             <img data-aos="fade-left" data-aos-duration="1500"  className='w-full h-full object-cover' src={img5} alt="/" />
-//         </div>
-//     </div>
-//     );
-// };
+const Gallery = () => {
+    return (
+        <div className='max-w-[1240px] mx-auto py-16 px-4 text-center'>
+        <h1 className='text-4xl lg:text-5xl font-bold text-[#D31027]'>OUR GALLERY</h1>
+        <p className='text-2xl py-4 font-bold'>Glance at our great image</p>
+        <div className='grid grid-rows-none md:grid-cols-5 py-4 gap-2 md:gap-4'>
+            <img data-aos="fade-right" data-aos-duration="1500" className='w-full h-full object-cover col-span-2 md:col-span-3 row-span-2' src={img1} alt="/" />
+            <img data-aos="fade-left" data-aos-duration="1500" className='w-full h-full object-cover' src={img2} alt="/" />
+            <img data-aos="fade-left" data-aos-duration="1500"  className='w-full h-full object-cover' src={img3} alt="/" />
+            <img data-aos="fade-left" data-aos-duration="1500"  className='w-full h-full object-cover' src={img4} alt="/" />
+            <img data-aos="fade-left" data-aos-duration="1500"  className='w-full h-full object-cover' src={img5} alt="/" />
+        </div>
+    </div>
+    );
+};
 
-// export default Gallery;
+export default Gallery;
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,11 +1,11 @@
 import Banner from "./Banner";
-import Faq from "./Faq";
+// import Faq from "./Faq";
 import Gallery from "./Gallery";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
-import Testimonial from "./Testimonial";
-import Footer from "./Footer";
+// import Testimonial from "./Testimonial";
+// import Footer from "./Footer";
 import { Helmet } from "react-helmet";
 
 const Home = () => {
@@ -22,9 +22,9 @@ const Home = () => {
       <div>
         <Banner></Banner>
         <Gallery></Gallery>
-        <Testimonial></Testimonial>
+        {/* <Testimonial></Testimonial>
         <Faq></Faq>
-        <Footer></Footer>
+        <Footer></Footer> */}
       </div>
     </div>
   );
