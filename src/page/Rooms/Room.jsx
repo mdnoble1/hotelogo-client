efc1601a872df7ccdd3735a98148a6763b21ce92
Md Noble <mdnoble.wd@gmail.com>
2023-11-08 13:49:03 +0600

    created cards to show room data

--- src/page/Rooms/Room.jsx
+++ src/page/Rooms/Room.jsx
@@ -1,11 +1,22 @@
-const Room = ({service}) => {
-
-    const {roomImage} = service;
-    return (
-        <div>
-            <img src={roomImage} alt="" />
-        </div>
-    );
+const Room = ({ service }) => {
+  const { roomType, roomImage, description, pricePerNight } = service;
+  return (
+    <div className="w-11/12 lg:w-[450px] rounded border border-gray-400 mx-auto">
+      <h2 className="font-semibold text-2xl text-black my-4 mx-6">
+        {roomType}
+      </h2>
+      <div className="">
+        <img className="w-full rounded-md h-64" src={roomImage} alt="" />
+      </div>
+      <p className="font-medium text-lg text-gray-600 my-4 mx-6">{description}</p>
+      <div className="flex justify-between items-center px-6 pb-10">
+        <button className="btn btn-outline rounded-lg font-semibold text-xl normal-case text-[#D31027]">
+          Details
+        </button>
+        <p className="font-semibold text-lg text-[#D31027]">{pricePerNight} / Night</p>
+      </div>
+    </div>
+  );
 };
 
 export default Room;
--- src/page/Rooms/Rooms.jsx
+++ src/page/Rooms/Rooms.jsx
@@ -3,30 +3,30 @@ import UseAxios from "../../hooks/UseAxios";
 import Room from "./Room";
 
 const Rooms = () => {
+  const axios = UseAxios();
 
+  const getServices = async () => {
+    const res = await axios.get("/services");
+    return res;
+  };
 
-    const axios = UseAxios();
+  const { data: services } = useQuery({
+    queryKey: ["service"],
+    queryFn: getServices,
+  });
 
-
-    const getServices = async() => {
-        const res = await axios.get('/services');
-        return res;
-    }
-
-    const { data : services } = useQuery({
-        queryKey: ['service'],
-        queryFn: getServices,
-    })
-
-
-
-    return (
-        <div>
-            {
-                services?.data?.map( service => <Room key={service._id} service={service}></Room>)
-            }
+  return (
+    <div>
+        <div className="mt-12">
+            <h2 className="font-bold text-2xl lg:text-5xl text-[#D31027] text-center">Our Featured Room Collection</h2>
+            <p className="font-medium lg:text-xl w-11/12 lg:w-1/2 text-center mx-auto my-4">Indulge in luxury and comfort with our carefully selected featured rooms. From breathtaking views to opulent decor, our handpicked accommodations promise an extraordinary stay. Your dream getaway is just a click away. Explore our featured rooms and experience the pinnacle of indulgence with HoteloGo</p>
         </div>
-    );
+      <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 my-12">
+        {services?.data?.map((service) => (
+          <Room key={service._id} service={service}></Room>
+        ))}
+      </div>
+    </div>
+  );
 };
-
 export default Rooms;
